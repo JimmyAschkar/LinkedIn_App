bbe2a496d8d0d4f51f9ab4a33a0f763daf8be7dc
JimmyAschkar <120240897+JimmyAschkar@users.noreply.github.com>
2022-12-13 21:20:09 -0600

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -169,7 +169,9 @@ person = [inc, educ, par, mar, gend, Ag]
 
 predicted_class = lr.predict([person])
 probs = lr.predict_proba([person])
-probs_percent = round(probs * 100)
+probs_percent = (probs * 100)
+probs_round = round(probs_percent)
+
 st.markdown("***")
    
 st.subheader("The Results Are In...")
@@ -178,9 +180,9 @@ if st.button('Predict'):
             if i == 1:
                 
                 st.subheader("The Model Predicts You ARE a LinkedIn User")
-                st.write(f"Probability Outcome: {probs_percent} %")
+                st.write(f"Probability Outcome: {probs_round} %")
             else:
                 st.subheader("The Model Predicts You ARE NOT a LinkedIn User ")
-                st.write(f"Probability Outcome: {probs_percent} %")
+                st.write(f"Probability Outcome: {probs_round} %")
 else:
     st.write("")
